20f4e4e21daf4de64f7e725f5e63d263ef7b7a3f
Sidhanta06 <143795565+Sidhanta06@users.noreply.github.com>
2023-11-18 09:58:37 +0530

    Update GithubPractice.java

--- GithubPractice/src/test/java/git/GithubPractice.java
+++ GithubPractice/src/test/java/git/GithubPractice.java
@@ -3,7 +3,7 @@ package git;
 public class GithubPractice {
 
 	public static void main(String[] args) {
-		System.out.println("Hi! my name is Git.");
+		System.out.println("Hi! mu git ru asichi.");
 	}
 
 }
